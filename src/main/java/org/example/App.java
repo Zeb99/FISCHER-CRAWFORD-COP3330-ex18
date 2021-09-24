46d402c23e53842607fd2f1ff44b1cd51eae15a1
Zeb99 <zeb.fischer-crawford@knights.ucf.edu>
2021-09-24 13:19:52 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,35 @@
+package org.example;
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Scanner scan = new Scanner(System.in);
+        int temp, converted_temp;
+
+        System.out.println("Press C to convert from Fahrenheit to Celsius.");
+        System.out.println("Press F to convert from Celsius to Fahrenheit.");
+        String input = scan.nextLine();
+        System.out.println("Your choice: " + input);
+
+        if (input.equalsIgnoreCase("C")){
+            System.out.println("Please enter the temperature in Fahrenheit");
+            temp = scan.nextInt();
+            converted_temp = ((temp - 32) * 5) / 9;
+            System.out.println("The temperature in Celsius is " + converted_temp);
+        } else if(input.equalsIgnoreCase("F")){
+            System.out.println("Please enter the temperature in Celsius");
+            temp = scan.nextInt();
+            converted_temp = ((temp * 9) / 5) + 32;
+            System.out.println("The temperature in Fahrenheit " + converted_temp);
+        } else {
+            System.out.println("Your input was not valid");
+        }
+
+    }
+}
